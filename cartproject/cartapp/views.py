1bfc05345c878e71cad56c343234a31aa8cb18f7
teguhsatyadharma <hiteguhsatya@gmail.com>
2021-11-12 20:20:06 +0800

    adding the views

--- cartproject/cartapp/views.py
+++ cartproject/cartapp/views.py
@@ -1,3 +1,16 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+def index(request):
+    return HttpResponse('Hello you are at index')
+
+def login(request):
+    return HttpResponse('Hello you are at login')
+
+def cart(request):
+    return HttpResponse('Hello you are at cart')
+
+def checkout(request):
+    return HttpResponse('Hello you are at checkout')
+
--- todos.md
+++ todos.md
@@ -1,3 +1,12 @@
 # CART SYSTEM TODOS
-[ ] Configure the URL and view
+## URLS and Views
+[x] Configure the URL and view
+- '/login' for login
+- '/' for product page
+- '/cart' for view cart
+- '/checkout' for checkout
+## Models
 [ ] Configure the model
+- login model
+- product model
+- invoice model
